67b63e0962b7526efadaea8dfe509d09f2e8dda4
MariolaBednarska <mariolabednarska96@gmail.com>
2023-02-07 20:34:03 +0100

    added github repo link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Mariola Bednarska and is{" "}
+        <a
+          href="https://github.com/MariolaBednarska/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>{" "}
+        on GitHub
+      </footer>
     </div>
   );
 }
-
-export default App;
