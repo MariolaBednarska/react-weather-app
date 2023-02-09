2daa1d23167b23add06fb70bf94fd897cc3b100a
MariolaBednarska <mariolabednarska96@gmail.com>
2023-02-09 20:48:14 +0100

    add Weather function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import Weather from './Weather';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Mariola Bednarska and is{" "}
-        <a
-          href="https://github.com/MariolaBednarska/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>{" "}
-        on GitHub
-      </footer>
+      <div className='container'>
+        <Weather />
+        <footer>
+          This project was coded by Mariola Bednarska and is{" "}
+          <a
+            href="https://github.com/MariolaBednarska/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>{" "}
+          on GitHub
+        </footer>
+      </div>
     </div>
   );
 }
