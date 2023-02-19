4284b3141eb7d278e6142068586ad64acddcdeed
MariolaBednarska <mariolabednarska96@gmail.com>
2023-02-19 21:45:43 +0100

    create WeatherTemperature.js component to change the unit of the temperature

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
     return (
@@ -15,13 +16,11 @@ export default function WeatherInfo(props) {
         <div className="row mt-3">
           <div className="col-6">
             <div className="clearfix">
-                <div className="float-left">
-                    <WeatherIcon code={props.data.icon} alt={props.data.description} />
-                </div>
-                <div className="float-left">
-                <span className="temperature">
-                  {Math.round(props.data.temperature)}
-                </span>
+              <div className="float-left">
+                <WeatherIcon code={props.data.icon} alt={props.data.description} />
+              </div>
+              <div className="float-left">
+                <WeatherTemperature celsius={props.data.temperature} />
               </div>
             </div>
           </div>
