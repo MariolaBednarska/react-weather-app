2a58b8375106a1fcbb01cef44f19e61341b91931
MariolaBednarska <mariolabednarska96@gmail.com>
2023-02-24 22:31:17 +0100

    update a footer and background of the page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import Weather from './Weather';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <div className='container'>
-        <Weather defaultCity="Warsaw" />
+      <div className="container">
+        <Weather defaultCity="Kraków" />
         <footer>
           This project was coded by Mariola Bednarska and is{" "}
           <a
             href="https://github.com/MariolaBednarska/react-weather-app"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
-            open-sourced
+            open-sourced on GitHub
           </a>{" "}
-          on GitHub
+          and{" "}
+          <a
+            href="https://lively-raindrop-203695.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
         </footer>
       </div>
     </div>
